b6512683535593ccee87231cb6d2c19f57d4875c
Daniel Zhou <danielzh021@gmail.com>
2025-01-21 00:02:13 -0800

    netlify.toml

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,16 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000
+  },
+  preview: {
+    port: 3000
+  },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[ext]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      },
-    },
-  },
-  base: '/',
+    sourcemap: true
+  }
 })
